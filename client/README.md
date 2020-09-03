a52fd85af552a75c7c67e9c2fe865b8d9b66746d
WLeondike <billyleondike@gmail.com>
2020-09-03 18:31:38 -0400

    post to uploads folder working

--- client/README.md
+++ client/README.md
@@ -1,3 +1,3 @@
 # React File Uploader
 
-In this application the user will be able to uplad a image and post it to our database
+In this application the user will be able to uplad a image
--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-const express = requre('express');
-const fileUpload = require('express-fileUpload');
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
 
 const app = express();
 
 app.use(fileUpload());
+app.use(cors());
 
 //Uplodad Endpoint
-app.post('/upload', (req, res) => {
-    if(req.files === null) {
-        return res.status(400).json({ msg: 'No file uploaded' });
+app.post("/upload", (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: "No file uploaded" });
+  }
+
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
     }
 
-    const file = req.files.file;
-    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
-        if(err) {
-            console.error(err);
-            return res.status(500).send(err);
-        }
-
-        res.json();
-    });
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
 });
 
-app.listen(5000, () => console.log('App listening on PORT: http://localhost:5000'))
+app.listen(5000, () => console.log("Started Server..."));
